fb30f00c62373589a0aa3fcb71f5a215de573677
adhyastanf <adhyasta.nf@gmail.com>
2024-01-17 20:25:22 +0700

    fix cookies

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,32 +3,31 @@
 import { getToken } from '@/app/actions/tokenAction';
 import axiosInstance from '@/utils/fetch';
 
-const HEADERS = (token) => {
+const HEADERS = async () => {
+  let getTokenAction = await getToken();
+  let token = await getTokenAction();
   return { headers: { Authorization: `Bearer ${token}` } };
 };
 
 const BASE_URL = 'https://gawaikuapi-production.up.railway.app';
 
-let getTokenAction = async () => await getToken();
-let token = getTokenAction();
-
 const getProfile = async () => {
-  const response = await fetch(`${BASE_URL}/auth/profile`, { ...HEADERS(token), next: { tags: ['profile'] }, cache: 'force-cache' });
+  const response = await fetch(`${BASE_URL}/auth/profile`, { ...HEADERS, next: { tags: ['profile'] }, cache: 'force-cache' });
   const data = await response.json();
   return data?.data;
 };
 
 const updateUserProfile = async (res, id) => {
-  const response = await axiosInstance.put(`/auth/user/${id}`, res, HEADERS(token));
+  const response = await axiosInstance.put(`/auth/user/${id}`, res, HEADERS);
   return response.data;
 };
 
 const registerUsers = async (res) => {
-  return await axiosInstance.post('/auth/registerUser', res, HEADERS(token));
+  return await axiosInstance.post('/auth/registerUser', res, HEADERS);
 };
 
 const loginUsers = async (res) => {
-  return await axiosInstance.post('/auth/loginUser', res, HEADERS(token));
+  return await axiosInstance.post('/auth/loginUser', res, HEADERS);
 };
 
 export { getProfile, updateUserProfile, registerUsers, loginUsers };
